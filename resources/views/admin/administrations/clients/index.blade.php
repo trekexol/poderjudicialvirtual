ef12bfbed259e67fbe3bc7d19e1f041395338865
trekexol <cefreitas.16@gmail.com>
2022-10-07 10:20:58 -0400

    sdf

--- resources/views/admin/administrations/clients/index.blade.php
+++ resources/views/admin/administrations/clients/index.blade.php
@@ -45,7 +45,7 @@
           @isset($clients)
             @foreach ($clients as $client)
             <tr>
-              <td>{{$client->countries['abbreviation'] ?? ''}}{{str_pad($client->id_client ?? 0, 6, "0", STR_PAD_LEFT)}}</td>
+              <td>{{$client->casillero}}</td>
               <td>{{$client->firstname}} {{$client->firstlastname}}</td>
               <td>{{$client->casillero}}</td>
               <td>{{$client->direction ?? ''}}</td>
